aaff36f9f7aab651500fd5fbe3bbbd748f904b07
Stackbit <projects@stackbit.com>
2023-05-03 19:45:56 +0000

    initial commit by Stackbit

---
+++ stackbit.config.ts
@@ -0,0 +1,29 @@
+import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from '@stackbit/cms-git';
+import { allModels } from 'sources/local/models';
+
+const gitContentSource = new GitContentSource({
+    rootPath: __dirname,
+    contentDirs: ['content'],
+    models: Object.values(allModels),
+    assetsConfig: {
+        referenceType: 'static',
+        staticDir: 'public',
+        uploadDir: 'images',
+        publicPath: '/'
+    }
+});
+
+export const config = defineStackbitConfig({
+    stackbitVersion: '~0.6.0',
+    ssgName: 'nextjs',
+    nodeVersion: '16',
+    styleObjectModelName: 'ThemeStyle',
+    contentSources: [gitContentSource],
+    presetSource: {
+        type: 'files',
+        presetDirs: ['sources/local/presets']
+    }
+});
+
+export default config;
