654005df9a76de5afe853836ae07eb47b55f181e
vitaliyrtest1 <51633367+vitaliyrtest1@users.noreply.github.com>
2023-08-23 10:53:30 -0400

    Controls

--- src/components/layouts/PageLayout/index.tsx
+++ src/components/layouts/PageLayout/index.tsx
@@ -8,6 +8,58 @@ export default function PageLayout(props) {
     const BaseLayout = getBaseLayoutComponent(page.baseLayout, site.baseLayout);
     const { title, sections = [] } = page;
 
+    const control1 = useSbControl({
+        type: 'string',
+        label: 'String field',
+        name: 'control1'
+    });
+    const control2 = useSbControl({
+        type: 'markdown',
+        label: 'Markdown field',
+        name: 'control2',
+        value: 'preset value'
+    });
+    const controlModel1 = useSbControl({
+        type: 'color',
+        label: 'Color field',
+        name: 'controlModel1',
+        context: 'model',
+        srcType: 'git',
+        srcProjectId: '9a0364b9',
+        modelName: 'PostLayout'
+    });
+    const controlDocument1 = useSbControl({
+        type: 'enum',
+        label: 'Enum field',
+        name: 'controlDocument1',
+        options: ['First', 'Second', 'Third'],
+        context: 'document',
+        srcType: 'git',
+        srcProjectId: '9a0364b9',
+        documentId: 'content/pages/index.md'
+    });
+    const controlField1 = useSbControl({
+        type: 'image',
+        label: 'Image field',
+        name: 'controlField1',
+        context: 'field',
+        srcType: 'git',
+        srcProjectId: '9a0364b9',
+        documentId: 'content/pages/index.md',
+        fieldPath: ['sections', 0]
+    });
+
+    React.useEffect(() => {
+        return () => {
+            window.stackbitControls = [];
+        };
+    }, []);
+
+    React.useEffect(() => {
+        window.stackbitControls = [control1, control2, controlModel1, controlDocument1, controlField1];
+
+    }, [control1, control2, controlModel1, controlDocument1, controlField1]);
+
     return (
         <BaseLayout page={page} site={site}>
             <main id="main" className="sb-layout sb-page-layout">
@@ -31,3 +83,21 @@ export default function PageLayout(props) {
         </BaseLayout>
     );
 }
+
+export const useSbControl = (controlBase: any) => {
+    const [value, setValue] = React.useState<any>(controlBase.value);
+    const [control, setControl] = React.useState({ ...controlBase });
+
+    React.useEffect(() => {
+        setControl((control) => ({
+            ...control,
+            value,
+            onChange(newValue) {
+                console.log(`value updated for ${control.name} to ${newValue}`);
+                setValue(newValue);
+            }
+        }))
+    }, [value]);
+
+    return control;
+};
